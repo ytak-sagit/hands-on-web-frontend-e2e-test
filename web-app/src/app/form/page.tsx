a83925d0963231c725c7e73217cbd276d832b22b
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-08-18 22:40:26 +0900

    test: 1.5.フォーム操作のテストを実装

--- web-app/src/app/form/page.tsx
+++ web-app/src/app/form/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import { ShuffleMemberForm } from "./form";
 
 export const metadata: Metadata = {
   title: "入力フォーム",
@@ -9,6 +10,7 @@ const Form = () => {
   return (
     <main>
       <h1>入力フォーム</h1>
+      <ShuffleMemberForm />
     </main>
   );
 };
