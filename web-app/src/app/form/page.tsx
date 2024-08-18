a97a792ba7b1543f544b798ffe7300ba0e56cbb6
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-08-18 22:12:41 +0900

    test: 1.4.ページ遷移のテストを実装

---
+++ web-app/src/app/form/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "入力フォーム",
+  description: "Playwrightハンズオンの第2のステップ",
+};
+
+const Form = () => {
+  return (
+    <main>
+      <h1>入力フォーム</h1>
+    </main>
+  );
+};
+
+export default Form;
--- web-app/src/app/layout.tsx
+++ web-app/src/app/layout.tsx
@@ -15,8 +15,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ja">
+      <body className={inter.className}>
+        <ul>
+          <li><a href='/'>ホーム</a></li>
+          <li><a href='/form'>入力フォーム</a></li>
+        </ul>
+        {children}
+      </body>
     </html>
   );
 }
