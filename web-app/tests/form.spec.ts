82f8c1bbbaff7a45698dedd27f6f8c30fddc9978
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2024-08-19 21:30:22 +0900

    test: スクリーンショットを撮影するテストを追加

--- web-app/tests/form.spec.ts
+++ web-app/tests/form.spec.ts
@@ -1,4 +1,7 @@
 import test, { expect } from "@playwright/test";
+import Jimp from "jimp";
+import dayjs from "dayjs";
+import { join } from "path";
 
 test("フォーム画面への遷移", async ({ page }) => {
   await page.goto("/");
@@ -17,4 +20,29 @@ test("フォーム操作のテスト", async ({ page }) => {
   await expect(page.getByRole("status", { name: /結果/ })).toHaveText(
     /(項羽→劉邦)|(劉邦→項羽)/
   );
+});
+
+test.only("スクリーンショット", async ({ page }, testInfo) => {
+  // ページ遷移してスクリーンショットを取得
+  await page.goto("/form");
+  const buffer = await page.screenshot();
+
+  // 取得したスクリーンショットを画像処理ライブラリで読み込み
+  const image = await Jimp.read(buffer);
+  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+
+  // スクリーンショットにテキストを書き込み、別ファイルに保存する
+  await image.print(
+    font,
+    0,
+    0,
+    {
+      alignmentX: Jimp.HORIZONTAL_ALIGN_RIGHT,
+      alignmentY: Jimp.VERTICAL_ALIGN_BOTTOM,
+      text: `${dayjs().format("YYYY/MM/DD HH:mm:ss")}`,
+    },
+    image.getWidth(),
+    image.getHeight()
+  );
+  await image.write(join(testInfo.outputDir, "screenshot_timestamp.png"));
 });
